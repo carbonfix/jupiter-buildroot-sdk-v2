a16910462c6d9173bb56a4e3207612483d5c1f2b
zhenglilang <lilang.zheng@spacemit.com>
2025-03-03 13:46:57 +0800

    k1:display: solve the issue of DSI open failure caused by LCD disconnected.
    Change-Id: I44a00e448d612c5692180e2eaaa430af15fdbef4

--- bsp-src/linux-6.6/drivers/gpu/drm/spacemit/raspberrypi_touchscreen.c
+++ bsp-src/linux-6.6/drivers/gpu/drm/spacemit/raspberrypi_touchscreen.c
@@ -406,7 +406,7 @@ static int rpi_touchscreen_probe(struct i2c_client *i2c)
 	ver = rpi_touchscreen_i2c_read(ts, REG_ID);
 	if (ver < 0) {
 		dev_err(dev, "Atmel I2C read failed: %d\n", ver);
-		return -ENODEV;
+		// return -ENODEV;
 	}
 
 	switch (ver) {
@@ -414,8 +414,8 @@ static int rpi_touchscreen_probe(struct i2c_client *i2c)
 	case 0xc3: /* ver 2 */
 		break;
 	default:
-	DRM_INFO("%s,Unknown Atmel firmware revision: 0x%02x\n", __func__, ver);
-		return -ENODEV;
+		DRM_INFO("%s,Unknown Atmel firmware revision: 0x%02x\n", __func__, ver);
+		// return -ENODEV;
 	}
 
 	/* Turn off at boot, so we can cleanly sequence powering on. */
