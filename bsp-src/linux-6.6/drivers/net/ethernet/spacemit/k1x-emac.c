5c9f4f4c06df1310f6a8913ccb563ec244b19aa1
Nell <xianbin.zhu@spacemit.com>
2025-03-22 12:48:00 +0800

    Revert "k1:emac: enable async suspend"
    we can't put the emac suspend callback into the suspend_noirq process
    which need irq enabled
    
    This reverts commit 6901b9e2dbce646e8aaa6bbc8171a93c35aa8662.
    
    Change-Id: Iefceb2aad7d4b3286eb219601c46a9943de719d2

--- bsp-src/linux-6.6/drivers/net/ethernet/spacemit/k1x-emac.c
+++ bsp-src/linux-6.6/drivers/net/ethernet/spacemit/k1x-emac.c
@@ -2837,7 +2837,6 @@ static int emac_probe(struct platform_device *pdev)
 	}
 	dma_set_mask_and_coherent(&pdev->dev, DMA_BIT_MASK(32));
 
-	device_enable_async_suspend(&pdev->dev);
 	netif_napi_add(ndev, &priv->napi, emac_rx_poll);
 
 	return 0;
@@ -2944,7 +2943,7 @@ static int emac_suspend(struct device *dev)
 #endif
 
 static const struct dev_pm_ops k1x_emac_pm_qos = {
-	NOIRQ_SYSTEM_SLEEP_PM_OPS(emac_suspend, emac_resume)
+	SET_SYSTEM_SLEEP_PM_OPS(emac_suspend, emac_resume)
 };
 
 static const struct of_device_id emac_of_match[] = {
